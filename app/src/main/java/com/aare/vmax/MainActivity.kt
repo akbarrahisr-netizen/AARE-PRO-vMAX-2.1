d56c74d54826fbdef52b2d1e18192f0a3695e114
akbarrahisr-netizen <akbarrahisr@gmail.com>
2026-04-25 01:38:20 +0530

    Update MainActivity.kt

--- app/src/main/java/com/aare/vmax/MainActivity.kt
+++ app/src/main/java/com/aare/vmax/MainActivity.kt
@@ -1,16 +1,16 @@
-package com.aare.vmax // ✅ पैकेज नाम छोटा
+package com.aare.vmax
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.aare.vmax.ui.screens.MainScreen
-import com.aare.vmax.ui.theme.VMaxTheme
+import com.aare.vmax.ui.theme.VMaxTheme // ✅ थीम इम्पोर्ट
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VMaxTheme { // आपका पर्पल थीम यहाँ काम करेगा
+            VMaxTheme { // ✅ थीम अप्लाई की
                 MainScreen()
             }
         }
