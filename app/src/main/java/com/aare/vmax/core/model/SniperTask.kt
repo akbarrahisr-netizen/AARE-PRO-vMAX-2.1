dd62ab35ce5b16ce3a8fcd88509cc8ebd369225a
akbarrahisr-netizen <akbarrahisr@gmail.com>
2026-04-24 23:47:39 +0530

    Replace PassengerData with SniperTask data class

--- app/src/main/java/com/aare/vmax/core/model/SniperTask.kt
+++ app/src/main/java/com/aare/vmax/core/model/SniperTask.kt
@@ -2,20 +2,14 @@ package com.aare.vmax.core.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import java.util.UUID
 
+// 🏹 इंजन के लिए असली स्नाइपर टास्क
 @Parcelize
-data class PassengerData(
-    val id: String = UUID.randomUUID().toString(),
-    var name: String = "",                   // 1. नाम
-    var age: String = "",                    // 2. उम्र
-    var gender: String = "Male",             // 3. जेंडर
-    var berthPref: String = "No Preference", // 4. सीट चॉइस
-    var mealPref: String = "No Food",        // 5. खाना
-    var bedRoll: Boolean = false             // 6. बेड रोल (Yes/No)
-) : Parcelable {
-    
-    // 🎯 इंजन को बताने के लिए कि फॉर्म भरा है या नहीं
-    fun isFilled() = name.isNotBlank() && age.isNotBlank()
-}
+data class SniperTask(
+    val taskId: String,
+    val trainNumber: String,
+    val travelClass: String,
+    val quota: String,
+    val passengers: List<PassengerData>
+) : Parcelable
 
