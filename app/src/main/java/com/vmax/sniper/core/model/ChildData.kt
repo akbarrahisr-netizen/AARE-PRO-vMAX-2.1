290b54db123e426e8d297b3e47564b2d63ac3135
akbarrahisr-netizen <akbarrahisr@gmail.com>
2026-05-04 11:53:52 +0530

    Update ChildData.kt

--- app/src/main/java/com/vmax/sniper/core/model/ChildData.kt
+++ app/src/main/java/com/vmax/sniper/core/model/ChildData.kt
@@ -4,10 +4,10 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import java.util.UUID
 
-@Parcelize
+@Parcelize  // ✅ यह होना चाहिए
 data class ChildData(
     val id: String = UUID.randomUUID().toString(),
     var name: String = "",
-    var ageRange: String = "Below 1 yr",
-    var gender: String = "Male"
-) : Parcelable
+    var ageRange: String = "",
+    var gender: String = ""
+) : Parcelable  // ✅ Parcelable implement होना चाहिए
